f2ecd589dc56d3bc36c0d8864fd606c04c8488eb
josephernest <nouvellecollection@gmail.com>
2019-12-04 12:24:20 +0100

    Using Crypto.Cipher.AES.MODE_GCM with authentification to easily test if wrong password entered or not

--- sdfgh.py
+++ sdfgh.py
@@ -1,51 +1,42 @@
 import tkinter, tkinter.messagebox, tkinter.simpledialog, tkinter.scrolledtext, Crypto.Random, Crypto.Protocol.KDF, Crypto.Cipher.AES
 import os; os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-def crypto(pwd, iv):
-    return Crypto.Cipher.AES.new(Crypto.Protocol.KDF.PBKDF2(pwd, iv, count=100000), Crypto.Cipher.AES.MODE_CFB, iv)
-
+def cipherAES_GCM(pwd, nonce):
+    key = Crypto.Protocol.KDF.PBKDF2(pwd, nonce, count=100000)
+    return Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_GCM, nonce=nonce, mac_len=16)
 def save(e):
     global pwd
     if 'pwd' not in globals(): 
         pwd = tkinter.simpledialog.askstring('sdfgh', 'Password:', show="*").encode()
-    iv = Crypto.Random.new().read(Crypto.Cipher.AES.block_size)
-    cipher = crypto(pwd, iv).encrypt(b'sdfgh' + text.get("1.0", 'end-1c').encode())  # add a header 'sdfgh' to be able to test if password ok when decrypting
+    nonce = Crypto.Random.new().read(16)
+    ciphertext = nonce + b''.join(cipherAES_GCM(pwd, nonce).encrypt_and_digest(text.get("1.0", 'end-1c').encode()))
     with open('sdfgh.dat', 'wb') as f:
-        f.write(iv + cipher)
+        f.write(ciphertext)
     text.edit_modified(False)
-
 def load():
     global pwd    
     try:
         with open('sdfgh.dat', 'rb') as f:
             s = f.read()
-        iv, cipher = s[:Crypto.Cipher.AES.block_size], s[Crypto.Cipher.AES.block_size:]
-        try:
-            root.update_idletasks()
-            pwd = tkinter.simpledialog.askstring('sdfgh', 'Password:', show="*").encode()
-        except AttributeError:
-            exit()
+        nonce, ciphertext, tag = s[:16], s[16:len(s)-16], s[-16:]
         while True:
-            s = crypto(pwd, iv).decrypt(cipher)
-            if s[:5] == b'sdfgh':  # is the header well decrypted? if not, wrong password!
-                break
             try:
                 root.update_idletasks()
-                pwd = tkinter.simpledialog.askstring('sdfgh', 'Wrong password, try again:', show="*").encode()
+                pwd = tkinter.simpledialog.askstring('sdfgh', 'Password:', show="*").encode()
+                s = cipherAES_GCM(pwd, nonce).decrypt_and_verify(ciphertext, tag)
+                break
             except AttributeError:
                 exit()
-        s = s[5:].decode()
+            except ValueError:  # wrong password, let's try again
+                pass
     except FileNotFoundError:  # new file
         s = "Welcome! This is a new file. Basic documentation:\n* Save with CTRL+S\n* Quit with CTRL+W or ALT+F4\n* Find a pattern with CTRL+F or F3"
     text.insert(tkinter.END, s)
     text.mark_set("insert", "1.0")  # cursor position at the start
     text.edit_modified(False)
     text.focus()
-
 def close(e=None):
     if not text.edit_modified() or tkinter.messagebox.askokcancel('sdfgh', "Some modifications have not been saved, do you really want to quit?", default=tkinter.messagebox.CANCEL):
         root.destroy()
-
 def find(e, findnext=False):
     global query
     if not findnext or 'query' not in globals():
@@ -57,7 +48,6 @@ def find(e, findnext=False):
     if pos != '':
         text.mark_set("insert", pos)
     text.focus()
-
 root = tkinter.Tk('sdfgh')  # root.withdraw()
 root.title('sdfgh')
 root.state('zoomed')
