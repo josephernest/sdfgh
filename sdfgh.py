b5fbab8b1489305ce805176df6abfc2626d43c87
josephernest <nouvellecollection@gmail.com>
2019-12-22 15:20:16 +0100

    move scrollbar with cursor when searching

--- sdfgh.py
+++ sdfgh.py
@@ -12,7 +12,7 @@ def save(e):
     with open('sdfgh.dat', 'wb') as f:
         f.write(ciphertext)
     text.edit_modified(False)
-def load():
+def load(root):
     global pwd    
     try:
         with open('sdfgh.dat', 'rb') as f:
@@ -30,10 +30,7 @@ def load():
                 pass
     except FileNotFoundError:  # new file
         s = "Welcome! This is a new file. Basic documentation:\n* Save with CTRL+S\n* Quit with CTRL+W or ALT+F4\n* Find a pattern with CTRL+F or F3"
-    text.insert(tkinter.END, s)
-    text.mark_set("insert", "1.0")  # cursor position at the start
-    text.edit_modified(False)
-    text.focus()
+    return s
 def close(e=None):
     if not text.edit_modified() or tkinter.messagebox.askokcancel('sdfgh', "Some modifications have not been saved, do you really want to quit?", default=tkinter.messagebox.CANCEL):
         root.destroy()
@@ -44,19 +41,25 @@ def find(e, findnext=False):
         start = "1.0"
     else:
         start = text.index(tkinter.INSERT) + "+1c"
-    pos = text.search(query, start, stopindex="end")
+    pos = text.search(query, start, stopindex="end", nocase=True)
     if pos != '':
         text.mark_set("insert", pos)
+        text.see("insert")
     text.focus()
-root = tkinter.Tk('sdfgh')  # root.withdraw()
-root.title('sdfgh')
-root.state('zoomed')
-text = tkinter.scrolledtext.ScrolledText(master=root, wrap='word', bg='#272822', fg="#f8f8f2", insertbackground='#f8f8f2', font=("Consolas", 10))
-text.pack(side="top", fill="both", expand=True, padx=0, pady=0)
-root.bind('<Control-s>', save)  # CTRL+S too?
-root.bind('<Control-w>', close)
-root.bind('<Control-f>', find)
-root.bind('<F3>', lambda e: find(e, True))
-root.protocol("WM_DELETE_WINDOW", close)
-load()
-root.mainloop()
+if __name__ == '__main__':
+    root = tkinter.Tk('sdfgh')
+    root.title('sdfgh')
+    root.state('zoomed')
+    text = tkinter.scrolledtext.ScrolledText(master=root, wrap='none', bg='#272822', fg="#f8f8f2", insertbackground='#f8f8f2', font=("Consolas", 10))
+    text.pack(side="top", fill="both", expand=True, padx=0, pady=0)
+    root.bind('<Control-s>', save)  # CTRL+S too?
+    root.bind('<Control-w>', close)
+    root.bind('<Control-f>', find)
+    root.bind('<F3>', lambda e: find(e, True))
+    root.protocol("WM_DELETE_WINDOW", close)
+    s = load(root)
+    text.insert(tkinter.END, s)
+    text.mark_set("insert", "1.0")  # cursor position at the start
+    text.edit_modified(False)
+    text.focus()
+    root.mainloop()
